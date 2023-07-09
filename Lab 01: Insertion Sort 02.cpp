50e0c6278d8d0bf5811240a15a06f797693acb05
Rabbi_Hasan <108593765+Rabbi-hasan0@users.noreply.github.com>
2023-07-10 03:39:39 +0600

    Descending Order with insertion sort

---
+++ Lab 01: Insertion Sort 02.cpp
@@ -0,0 +1,35 @@
+/**    Don't Copy Code from anywhere
+**     Author: Rabbi Hasan
+**     Bangladesh University of Business and Technology,
+**     Dept. of CSE.
+***/
+#include<bits/stdc++.h>
+#define nn "\n"
+using namespace std;
+void insertionSort(int a[], int n)
+{
+    int i,key,j;
+    for (i=1; i<n; i++)
+    {
+        key=a[i];
+        j=i-1;
+        while (j>=0 && a[j]<key)
+        {
+            a[j+1]=a[j];
+            j--;
+        }
+        a[j+1]=key;
+    }
+    for (i=0; i<n; i++)
+        cout<<a[i]<<' ';
+    cout<<nn;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n+1];
+    for(int i=0; i<n; i++)
+        cin>>a[i];
+    insertionSort(a,n);
+}
