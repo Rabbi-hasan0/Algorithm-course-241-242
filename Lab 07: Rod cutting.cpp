fe0af7a6b7ca9910239ae7800a2a75546f7a072e
Rabbi_Hasan <108593765+Rabbi-hasan0@users.noreply.github.com>
2023-08-21 00:39:50 +0600

    Rod cutting

--- Lab 07: Rod cutting.cpp
+++ Lab 07: Rod cutting.cpp
@@ -3,8 +3,8 @@ using namespace std;
 /*
 input:
 5
-1 2 3 4
-2 3 6 9
+1 2 3 4 5
+2 3 6 9 1
 output:
 0 0 0 0
 0 2 4 6
@@ -17,41 +17,38 @@ int main()
     int l;
     cin >> l;
     int c[5],p[5];
-    for(int i=0; i<4; i++){
-    cin>>c[i];
-     }
-    for(int i=0; i<4; i++){
+    for(int i=0; i<l; i++){
     cin>>p[i];
      }
 
-    for(int i=0; i<4; i++){
-    for(int j=0; j<4; j++){
+    for(int i=0; i<=l; i++){
+    for(int j=0; j<=l; j++){
       if(i==0 || j==0){
        dp[i][j]=0;
       }
       else{
        if(i==1){
-        dp[i][j]=j*p[i-1];
+        dp[i][j] = j*p[i-1];
        }
        else{
          if(i>j){
            dp[i][j]=dp[i-1][j];
          }
          else{
-          dp[i][j]=max(p[i-1]+dp[i][j-1], dp[i-1][j]);
+          dp[i][j]=max(p[i-1]+dp[i][j-i], dp[i-1][j]);
          }
        }
       }
     }
     }
-    for(int i=0; i<4; i++){
-    for(int j=0; j<4; j++){
+    for(int i=0; i<=l; i++){
+    for(int j=0; j<=l; j++){
       cout<<dp[i][j]<<' ';
      }
      cout<<endl;
     }
    cout<<endl;
 
- cout<<"MAX ANS: "<<dp[3][3]<<'\n';
+ cout<<"MAX ANS: "<<dp[l][l]<<'\n';
  return 0;
 }
