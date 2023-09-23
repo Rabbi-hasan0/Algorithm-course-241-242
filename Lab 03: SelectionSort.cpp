e63f975294e909b77c499c5c0d5f4bde7c3579c3
Rabbi_Hasan <108593765+Rabbi-hasan0@users.noreply.github.com>
2023-09-24 00:51:27 +0600

    SelectionSort Algo

--- Lab 03: SelectionSort.cpp
+++ Lab 03: SelectionSort.cpp
@@ -3,19 +3,18 @@
 **     Bangladesh University of Business and Technology,
 **     Dept. of CSE.
 ***/
+
+//-> Just finding min element and swaping first index and doing index++;
 #include<bits/stdc++.h>
 using namespace std;
-void insertion(int a[],int n)
-{
+void insertion(int a[],int n){
   int M;
-  for(int i=0; i<n-1; i++)
-  {
+  for(int i=0; i<n-1; i++){
     M=i;
-    for(int j=i+1; j<n; j++)
-    {
+    for(int j=i+1; j<n; j++){
       if(a[M]>a[j])
         M=j;
-    }
+     }
     if(M!=i)
       swap(a[M],a[i]);
   }
