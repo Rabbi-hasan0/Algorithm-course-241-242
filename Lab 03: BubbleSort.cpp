6e2e5c2e733870e54edf23ea3a96f3f17039f2f7
Rabbi_Hasan <108593765+Rabbi-hasan0@users.noreply.github.com>
2023-10-04 02:24:15 +0600

    BubbleSort

---
+++ Lab 03: BubbleSort.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#define int long long
+using namespace std;
+
+
+int32_t main(){
+
+    int n;
+    cin >> n;
+    int a[n];
+    for(int i=0; i<n; i++){
+      cin >> a[i];
+    }
+
+    for(int i=0; i<n-1; i++){
+     for(int j=0; j<n; j++){
+       if (a[j] > a[j+1]){
+         swap(a[j], a[j+1]);
+       }
+     }
+     /*for(auto it: a){
+      cout << it << ' ';
+    }cout << '\n';*/
+    }
+     
+    for(auto it: a){
+      cout << it << ' ';
+    }cout << '\n';
+return 0;
+}
